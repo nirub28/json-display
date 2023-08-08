8a3775245306e6cf674a2560b84b8b22cf07d3e1
Niranjan P B <niranjanbgowda123@gmail.com>
2023-08-09 00:06:34 +0530

    working code

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("./assets"));
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('index'); 
 });
 
 app.post('/submit', (req, res) => {
     const submittedJson = req.body.json_input;
-    res.json(submittedJson);
+    const jsonData = JSON.parse(submittedJson);
+    res.render('json-data', { submittedJson, jsonData }); 
+});
+
+app.get('/json-data', (req, res) => {
+    const jsonData = req.query.data;
+    res.render('json-data', { jsonData });
 });
 
 app.listen(port, () => {
