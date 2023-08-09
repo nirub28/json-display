5d34170e39d774db330c087356327cedc2646b4a
Niranjan P B <niranjanbgowda123@gmail.com>
2023-08-09 23:40:04 +0530

    final test1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,7 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.use(express.static("./assets"));
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
